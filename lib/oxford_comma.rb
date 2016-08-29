2d28be28b3921c8af09a87a4e5242401bcef5e4f
Drew Nickerson <drew.nickerson@flatironschool.com>
2016-08-29 18:10:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(' and ')
+  else array.length == 3
+    last = array.pop
+    new_last = "and #{last}"
+    array << new_last
+    array.join(', ')
+  end
 end
+
+test_array = ["kiwi", "durian", "starfruit"]
+puts oxford_comma(test_array)
